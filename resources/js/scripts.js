5f72caae750bd80a41469496fa80121b783ddfcd
Terces <minhduc231298@gmail.com>
2020-04-12 20:14:32 +0700

    updated

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -21,3 +21,29 @@ $(document).ready(
         });
     }
 )
+
+const CORRECT_EMAIL = 'admin@gmail.com';
+const CORRECT_PASSWORD = '123';
+
+var inputEmail = document.getElementById('email');
+var inputPassword = document.getElementById('password');
+
+var formLogin = document.getElementById('form-login');
+
+if(formLogin.attachEvent){
+    formLogin.attachEvent('submit',onFormSubmit);
+}else{
+    formLogin.addEventListener('submit',onFormSubmit);
+}
+
+function onFormSubmit(e){
+    var email = inputEmail.value;
+    var password = inputPassword.value;
+
+    if(email == CORRECT_EMAIL && password == CORRECT_PASSWORD ){
+        alert('Đăng nhập thành công');
+    }else{
+        alert('Tài khoản hoặc mật khẩu sai');
+    }
+}
+
